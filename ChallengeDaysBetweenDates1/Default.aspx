5ee960caac709f3ae0ca600e7f4d362584b4d55a
Annpettway <Brokernajem@Yaoo.com>
2017-03-19 03:01:13 -0500

    Add project files.

---
+++ ChallengeDaysBetweenDates1/Default.aspx
@@ -0,0 +1,33 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="ChallengeDaysBetweenDates1.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        How many Days have elapsed?<br />
+        <br />
+        Choose one date:<br />
+        <asp:Calendar ID="Calendar1" runat="server" BackColor="Yellow" BorderStyle="Double"></asp:Calendar>
+        <br />
+        <br />
+        <br />
+        Choose a second date:<br />
+        <asp:Calendar ID="Calendar2" runat="server" BackColor="#00CC00" BorderStyle="Dashed"></asp:Calendar>
+        <br />
+        <asp:Button ID="okButton" runat="server" OnClick="Button1_Click" Text="OK" />
+        <br />
+        <br />
+        <asp:Label ID="resultLabel" runat="server"></asp:Label>
+        <br />
+        <br />
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ ChallengeDaysBetweenDates1/Default.aspx.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ChallengeDaysBetweenDates1
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        public object days { get; private set; }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+                  
+                                         
+
+          DateTime firstDate = Calendar1.SelectedDate;
+          int dayone = firstDate.Day;
+
+          DateTime secondDate = Calendar2.SelectedDate;
+          int dayTwo = secondDate.Day;
+
+
+          TimeSpan ts = TimeSpan.Parse("1");
+
+              if (Calendar1.SelectedDate > Calendar2.SelectedDate)
+                  ts = Calendar1.SelectedDate - Calendar2.SelectedDate;
+
+              else
+                  ts = Calendar2.SelectedDate - Calendar1.SelectedDate;
+
+              resultLabel.Text = ts.Days.ToString();
+         
+
+         }
+    }
+}
